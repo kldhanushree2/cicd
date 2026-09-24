bb0de09d5ffb80199c3999f4b00723621cc9a348
Dhanushree <kldhanushree2@outlook.com>
2026-09-24 09:35:14 +0530

    failed test

--- test_factorial.py
+++ test_factorial.py
@@ -3,7 +3,7 @@ from factorial import factorial
 def test_factorial_of_zero():
     assert factorial(0) == 1
 def test_factorial_of_five():
-    assert factorial(5) == 120
+    assert factorial(5) == 121
 def test_factorial_of_ten():
     assert factorial(10) == 3628800
 def test_negative_number():
